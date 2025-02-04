e27ae57396f7a5a4ec0afc6b383893282b0b9df0
Eduarda Vieira <159597766+eduardavieira-dev@users.noreply.github.com>
2025-02-04 12:29:41 -0300

    init commit

---
+++ meaven-build-tool/src/main/java/br/lmp/Calculadora.java
@@ -0,0 +1,23 @@
+package br.lmp;
+
+    public class Calculadora {
+        public int somar(int a, int b) {
+            return a + b;
+        }
+    
+        public int subtrair(int a, int b) {
+            return a - b;
+        }
+    
+        public int multiplicar(int a, int b) {
+            return a * b;
+        }
+    
+        public double dividir(int a, int b) {
+            if (b == 0) {
+                throw new IllegalArgumentException("Divisor não pode ser zero");
+            }
+            return (double) a / b;
+        }
+    }
+
